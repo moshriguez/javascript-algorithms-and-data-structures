8abfae7ee75322ad12dba075496c7e263335af27
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-10-27 01:18:02 -0700

    added factorial

--- recursion.js
+++ recursion.js
@@ -12,3 +12,14 @@ function oddNumber(arr) {
 console.log(oddNumber([3142, 5798, 6550, 5914]))
 console.log(oddNumber([3142, 5798, 6551, 5914]))
 console.log(oddNumber([]))
+
+function factorial(num) {
+    if(num === 1) {
+        return 1
+    }
+    return num * factorial(num - 1)
+}
+
+console.log(factorial(3))
+console.log(factorial(6))
+console.log(factorial(2))
