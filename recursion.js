5f5b38b23476fcb440a155d7c41e1522680f47ff
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-10-27 01:35:40 -0700

    recusive helper methods!! what an epiphany!

--- recursion.js
+++ recursion.js
@@ -23,3 +23,26 @@ function factorial(num) {
 console.log(factorial(3))
 console.log(factorial(6))
 console.log(factorial(2))
+
+// Recursive helper method:
+function collectOddValues(arr) {
+    const result = []
+
+    function helper(input) {
+        if (input.length === 0) {
+            return
+        }
+        if (input[0] % 2 !== 0) {
+            result.push(input[0])
+        }
+        helper(input.slice(1))
+    }
+
+    helper(arr)
+
+    return result
+}
+
+console.log(collectOddValues([1,2,3,4,5,6,7,8,9,10,11,13]))
+console.log(collectOddValues([2,4,6,8,10,13]))
+console.log(collectOddValues([4,2]))
