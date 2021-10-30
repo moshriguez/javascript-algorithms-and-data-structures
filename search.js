47ec5fa2a6b35dd5c4fe797b03a8009cf19b7801
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-10-30 13:03:32 -0700

    binary search refactored and added instructor solution

--- search.js
+++ search.js
@@ -12,24 +12,36 @@ const linearSearch = (arr, ele) => {
 // console.log(linearSearch([3, 2, 3,], 3))
 
 // Binary Search
-
+// my solution
 const binarySearch = (arr, ele) => {
-    if (arr.length === 0) return -1
     let left = 0
     let right = arr.length - 1
     while(left <= right) {
-        let mid = Math.floor((right - left) / 2) + left
-        console.log('mid', mid)
-        if (ele === arr[mid]) {return mid}
-        if (ele < arr[mid]) {
-            right = mid - 1
-        } else {
-            left = mid + 1
-        }
+        let mid = Math.floor((right + left) / 2)
+        if (ele === arr[mid]) return mid;
+        if (ele < arr[mid]) right = mid - 1;
+        else left = mid + 1;
     }
     return -1
 }
 
-console.log(binarySearch([1,2,3,4,5,65], 4))
-console.log(binarySearch([1,2,3,4,5,65], 0))
-console.log(binarySearch([1,2,3,4,5,65], 65))
+console.log(binarySearch([1,2,3,4,5,65], 4)) // 3
+console.log(binarySearch([1,2,3,4,5,65], 0)) // -1
+console.log(binarySearch([1,2,3,4,5,65], 65)) // 5
+
+// instructor solution
+function search_binary(arr, ele) {
+    let start = 0
+    let end = arr.length - 1
+    let mid = Math.floor((start + end) / 2)
+    while(arr[mid] !== ele && start <= end) {
+        if (ele < arr[mid]) end = mid - 1;
+        else start = mid + 1;
+        mid = Math.floor((start + end) / 2);
+    }
+    return arr[mid] === ele ? mid : -1
+}
+
+console.log(search_binary([2, 4, 6, 7, 8, 11, 23, 34, 45], 4)) // 1
+console.log(search_binary([2, 6, 7, 8, 11, 23, 34, 45], 4)) // -1
+console.log(search_binary([2, 4, 6, 7, 8, 11, 23, 34, 45], 45)) // 8 
