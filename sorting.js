4171d8b6115fa11a9ab934cc5a347a482d3be42a
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-01 12:28:25 -0700

    add short circuit to break loop when array is sorted

--- sorting.js
+++ sorting.js
@@ -1,14 +1,20 @@
 // Bubble Sort
 
 function bubbleSort(arr) {
+    let noSwaps
     for(let i = arr.length; i>=0; i--) {
+        noSwaps = true
         for(let j=0; j<i-1; j++) {
+            console.log(arr, arr[j], arr[j+1])
             if(arr[j]>arr[j+1]){
                 [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+                noSwaps = false
             }
         }
+        if(noSwaps) break;
     }
     return arr
 }
 
 console.log(bubbleSort([1,3,4,2,6,8,2,3]))
+console.log(bubbleSort([8,1,2,3,4,5,6,7]))
