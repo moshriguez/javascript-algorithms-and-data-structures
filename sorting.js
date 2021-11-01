5412c3cfb07337bb6970f9c2b7b05eb0178b8b0a
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-01 15:15:10 -0700

    add selection sort

--- sorting.js
+++ sorting.js
@@ -16,5 +16,25 @@ function bubbleSort(arr) {
     return arr
 }
 
-console.log(bubbleSort([1,3,4,2,6,8,2,3]))
-console.log(bubbleSort([8,1,2,3,4,5,6,7]))
+// console.log(bubbleSort([1,3,4,2,6,8,2,3]))
+// console.log(bubbleSort([8,1,2,3,4,5,6,7]))
+
+// Selection Sort
+
+function selectionSort(arr) {
+    for(let i = 0; i<arr.length; i++) {
+        let min = i
+        for(let j = i+1; j<arr.length; j++) {
+            if(arr[j] < arr[min]) {
+                min = j
+            }
+        }
+        if(min !== i) {
+            [arr[i], arr[min]] = [arr[min], arr[i]]
+        }
+    }
+    return arr
+}
+
+console.log(selectionSort([1,3,4,2,6,8,2,3]))
+console.log(selectionSort([8,1,2,3,4,5,6,7]))
