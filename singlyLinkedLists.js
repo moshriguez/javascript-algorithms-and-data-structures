2b4aab4b3a29b0af351029e75d8302e0158a5164
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-03 14:37:28 -0700

    start singly linked list class with basic methods

---
+++ singlyLinkedLists.js
@@ -0,0 +1,55 @@
+// piece of data - val
+// reference to next node - node
+
+class Node{
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class SinglyLinkedList{
+    constructor() {
+        this.length = 0
+        this.head = null
+        this.tail = null
+    }
+    push(val){
+        const newNode = new Node(val)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = this.head
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+    print() {
+        let current = this.head
+        while(current) {
+            console.log(current.val)
+            current = current.next
+        }
+    }
+    pop() {
+        if(!this.head) {
+            return undefined
+        }
+        let current = this.head
+        let previous = current
+        while(current.next) {
+            previous = current
+            current = current.next
+        }
+        this.tail = previous
+        this.tail.next = null
+        this.length--
+        if(this.length === 0) {
+            this.head = null
+            this.tail = null
+        }
+        return current
+    }
+}
