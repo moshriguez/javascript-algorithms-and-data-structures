ef6169412cdd461a99f5b3df898c3ba8ac1dd9e8
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-04 23:00:47 -0700

    add unshift method

--- doublyLinkedLists.js
+++ doublyLinkedLists.js
@@ -26,34 +26,61 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+        this.head.prev = newNode
+        newNode.next = this.head
+        this.head = newNode
+    }
+    this.length++
+    return this
+  }
   // Deletion
   pop() {
-      if(!this.tail) return undefined
-      let oldTail = this.tail
-      if(this.length === 1) {
-          this.head = null
-          this.tail = null
-      } else {
-          this.tail = oldTail.prev
-          this.tail.next = null
-          oldTail.prev = null
-      }
-      this.length--
-      return oldTail
+    if (!this.tail) return undefined;
+    let oldTail = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = oldTail.prev;
+      this.tail.next = null;
+      oldTail.prev = null;
+    }
+    this.length--;
+    return oldTail;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+        this.head = null;
+        this.tail = null;
+    } else {
+        this.head = oldHead.next;
+        this.head.prev = null;
+        oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
   }
   // Utilities
   print() {
-      let current = this.head
-      while(current) {
-          console.log(current.val)
-          current = current.next
-      }
+    let current = this.head;
+    while (current) {
+      console.log(current.val);
+      current = current.next;
+    }
   }
 }
 
-let list = new DoublyLinkedList
-list.push(1)
-list.push(2)
-list.push(3)
-list.push(4)
-list.push(5)
+let list = new DoublyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
