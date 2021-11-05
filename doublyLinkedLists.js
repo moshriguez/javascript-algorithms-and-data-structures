bed7e273a4b002feafaff367dee7abee33313286
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-04 22:27:37 -0700

    add pop method to doubly linked list

--- doublyLinkedLists.js
+++ doublyLinkedLists.js
@@ -12,6 +12,7 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  // Insertion
   push(val) {
     let newNode = new Node(val);
     if (!this.head) {
@@ -25,4 +26,34 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  // Deletion
+  pop() {
+      if(!this.tail) return undefined
+      let oldTail = this.tail
+      if(this.length === 1) {
+          this.head = null
+          this.tail = null
+      } else {
+          this.tail = oldTail.prev
+          this.tail.next = null
+          oldTail.prev = null
+      }
+      this.length--
+      return oldTail
+  }
+  // Utilities
+  print() {
+      let current = this.head
+      while(current) {
+          console.log(current.val)
+          current = current.next
+      }
+  }
 }
+
+let list = new DoublyLinkedList
+list.push(1)
+list.push(2)
+list.push(3)
+list.push(4)
+list.push(5)
