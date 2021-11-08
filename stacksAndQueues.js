b174038bb811f1318640561e451856cb60fe4e78
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-07 22:31:55 -0800

    added queue implementation via array and class

--- stacksAndQueues.js
+++ stacksAndQueues.js
@@ -17,6 +17,7 @@ stack.shift()
 stack.shift()
 stack.shift()
 // however push/pop is better BigO due to the reindexing that occurs with unshift/shift
+// using an array with push/pop for a stack gives the same BigO as using the below class implementation
 
 // implementation via a singly linked list class
 class Node {
@@ -45,6 +46,72 @@ class Stack {
         return ++this.size
     }
     pop() {
+        if(!this.first) return null
+        let node = this.first
+        if(this.size === 1) {
+            this.last = null
+        }
+        this.first = node.next
+        node.next = null
+        this.size--
+        return node.val
+    }
+}
+
+// Queues
+// FIFO - First In First Out
+// implementation via an array:
+// this is not ideal as there is no way around reindexing the entire array;
+// whether we use push/shift or unshift/pop, we have to reindex the array, so the class implementation below is always better BigO
+let q = []
+q.push('first')
+q.push('second')
+q.push('third')
+// q = ['first', 'second', 'third']
+q.shift()
+// q = ['second', 'third']
+q.shift()
+// q = ['third']
+q.shift()
+// q = []
+q.unshift('first')
+q.unshift('second')
+q.unshift('third')
+// q = ['third', 'second', 'first']
+q.pop()
+// q = ['third', 'second']
+q.pop()
+// q = ['third']
+q.pop()
+
+// implementation using a class:
+class Node {
+    constructor(val) {
+        this.val = val
+        this.next = null
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null
+        this.last = null
+        this.size = 0
+    }
+    // enqueue is push from SLL (singly linked list)
+    enqueue(val) {
+        let newNode = new Node(val)
+        if(!this.first) {
+            this.first = newNode
+            this.last = newNode
+        } else {
+            this.last.next = newNode
+            this.last = newNode
+        }
+        return ++this.size
+    }
+    // dequeue is shift from SLL
+    dequeue() {
         if(!this.first) return null
         let node = this.first
         if(this.size === 1) {
