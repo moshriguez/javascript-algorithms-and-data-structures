91008e858d0d7b1b5f42d2e80aab279fce248793
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-08 22:42:55 -0800

    add insert method

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -15,4 +15,44 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let current = this.root;
+      while (current.val !== val) {
+        if (current.val === val) return undefined;
+        if (val > current.val) {
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = newNode;
+            current = current.right;
+          }
+        } else {
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = newNode;
+            current = current.left;
+          }
+        }
+      }
+    }
+    return this;
+  }
 }
+
+//      10
+//   5     13
+// 2  7  11  16
+
+var tree = new BinarySearchTree();
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
