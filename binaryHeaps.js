45c8f839635da9791882c64883c71b53d6481268
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-10 12:44:39 -0800

    start binary heaps

---
+++ binaryHeaps.js
@@ -0,0 +1,13 @@
+// Binary Heaps
+// very similar to binary search tree
+// each parent has at most 2 child nodes
+// all children of each node are as full as they can be and left children are filled out first
+// no implied ordering between children
+// Max Binary Heap - parent nodes are always LARGER than child nodes
+// Min Binary Heap - parent nodes are always SMALLER than child nodes
+
+class MaxBinaryHeap {
+    constructor() {
+        this.values = []
+    }
+}
