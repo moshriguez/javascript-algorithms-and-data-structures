de9c15092c5df090414df6f45dda4bc3c56b1f94
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-10 15:34:40 -0800

    add extract max

--- binaryHeaps.js
+++ binaryHeaps.js
@@ -19,8 +19,62 @@ class MaxBinaryHeap {
         while(idx > 0) {
             let parentIdx = Math.floor((idx-1)/2)
             if(this.values[idx] <= this.values[parentIdx]) break;
-            [this.values[idx], this.values[parentIdx]] = [this.values[parentIdx], this.values[idx]]
+            this.swap(idx, parentIdx)
             idx = parentIdx
+        }
+    }
+    swap(i1, i2) {
+        [this.values[i1], this.values[i2]] = [this.values[i2], this.values[i1]]
+    }
+    extractMax() {
+        if(!this.values.length) return undefined
+        this.swap(0, this.values.length-1)
+        const max = this.values.pop()
+        this.trickleDown()
+        return max
+    }
+    trickleDown() {
+        let idx = 0, leftIdx = 1, rightIdx = 2
+        // Math.max returns NaN if one of the args is undefined
+        let max = Math.max(this.values[leftIdx], this.values[rightIdx] || -Infinity)
+        // if the parent is greater than both children or has no children, were done
+        while(this.values[idx] < max) {
+            let child = this.values[leftIdx] === max ? leftIdx : rightIdx
+            this.swap(idx, child)
+            idx = child
+            leftIdx = 2*idx + 1
+            rightIdx = 2*idx + 2
+            max = Math.max(this.values[leftIdx], this.values[rightIdx] || -Infinity)
+        }
+    }
+    // instructor's code - can replace trickleDown with sinkDown
+    sinkDown() {
+        let idx = 0
+        const length = this.values.length
+        const element = this.values[0]
+        while(true) {
+            let leftIdx = 2*idx + 1
+            let rightIdx = 2*idx + 2
+            let leftChild, rightChild
+            let swap = null
+            if (leftIdx < length) {
+                leftChild = this.values[leftIdx]
+                if(leftChild > element) {
+                    swap = leftIdx
+                }
+            }
+            if (rightIdx < length) {
+                rightChild = this.values[rightIdx]
+                if(
+                    (swap === null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild)
+                 ) {
+                    swap = rightIdx
+                }
+            }
+            if(!swap) break
+            this.swap(idx, swap)
+            idx = swap
         }
     }
 }
