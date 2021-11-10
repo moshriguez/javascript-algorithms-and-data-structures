ba9ea1fbe2c8de603b25646647b1c9569faeb3b6
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-10 13:25:57 -0800

    add insert method

--- binaryHeaps.js
+++ binaryHeaps.js
@@ -10,4 +10,26 @@ class MaxBinaryHeap {
     constructor() {
         this.values = []
     }
+    insert(val) {
+        this.values.push(val)
+        this.bubbleUp()
+    }
+    bubbleUp() {
+        let idx = this.values.length - 1
+        while(idx > 0) {
+            let parentIdx = Math.floor((idx-1)/2)
+            if(this.values[idx] <= this.values[parentIdx]) break;
+            [this.values[idx], this.values[parentIdx]] = [this.values[parentIdx], this.values[idx]]
+            idx = parentIdx
+        }
+    }
 }
+
+let heap = new MaxBinaryHeap
+heap.insert(41)
+heap.insert(39)
+heap.insert(33)
+heap.insert(18)
+heap.insert(27)
+heap.insert(12)
+heap.insert(55)
