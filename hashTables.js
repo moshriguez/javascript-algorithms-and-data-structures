13de1872656eb64d1943ec283f3809c335098e7d
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-11 22:32:34 -0800

    slightly better hash function

--- hashTables.js
+++ hashTables.js
@@ -1,9 +1,13 @@
+
+
 // hash function
 function hash(key, arrayLen) {
     let total = 0
-    for (let char of key) {
+    let prime = 31
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+        let char = key[i]
         let value = char.charCodeAt(0) - 96
-        total += value
+        total += value + prime
     }
     return total % arrayLen
 }
