9fa870ae82c032fcbbd3adc5e959933f422fbeb1
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-11 23:11:12 -0800

    add hash table from scratch with get and set

--- hashTables.js
+++ hashTables.js
@@ -1,14 +1,44 @@
-
-
-// hash function
-function hash(key, arrayLen) {
-    let total = 0
-    let prime = 31
-    for (let i = 0; i < Math.min(key.length, 100); i++) {
-        let char = key[i]
-        let value = char.charCodeAt(0) - 96
-        total += value + prime
+class HashTable {
+    constructor(size=53) {
+        this.keyMap = new Array(size)
     }
-    return total % arrayLen
+
+    // hash function
+    _hash(key) {
+        let total = 0
+        let prime = 31
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96
+            total += value + prime
+        }
+        return total % this.keyMap.length
+    }
+
+    set(key, value) {
+        const idx = this._hash(key)
+        if(!this.keyMap[idx]) {
+            this.keyMap[idx] = []
+        }
+        this.keyMap[idx].push([key, value])
+    }
+    
+    get(key) {
+        const idx = this._hash(key)
+        if (!this.keyMap[idx]) return undefined
+        for(let item of this.keyMap[idx]){
+            if (item[0] === key) {
+                return item[1]
+            }
+        }
+        return undefined
+        // this.keyMap[idx].find(ele => ele[0] === key)
+    }
 }
-// hash('pink', 100)
+
+let ht = new HashTable(5)
+ht.set('name', 'Marc')
+ht.set('age', '36')
+ht.set('hair', 'brown')
+ht.set('eyes', 'blue')
+ht.set('height', `5'8"`)
