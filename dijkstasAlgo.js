b47d0556c0587e7a719de202fa2cc09a33918404
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-11-15 15:56:00 -0800

    start dijkstas algo with weighted graph and priority queue

---
+++ dijkstasAlgo.js
@@ -0,0 +1,33 @@
+// Dijkstra's Shortest Path Algorithm
+// start with a weighted graph:
+
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+    // {'a': [{node: 'b', weight: 10}], {node: 'c', weight: 5}}
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(v1, v2, weight) {
+    this.adjacencyList[v1].push({ node: v2, weight });
+    this.adjacencyList[v2].push({ node: v1, weight });
+  }
+}
+
+// we also will need a Priority Queue:
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
